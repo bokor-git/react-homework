53afaf7ad6f656b8e2856907361f15d7a61f2078
bokor-git <bogdankorolbk@gmail.com>
2020-07-19 21:29:08 +0300

    Updates

---
+++ precache-manifest.544a8f117e98a4e677c95ceae2a910df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc98f4babd1796a3ed3c1add009d9e2",
+    "url": "/react-homework/index.html"
+  },
+  {
+    "revision": "bb81ce7b8ddecb3dca99",
+    "url": "/react-homework/static/css/main.dd1bca4e.chunk.css"
+  },
+  {
+    "revision": "10e9177cba3a4760071a",
+    "url": "/react-homework/static/js/2.d618cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework/static/js/2.d618cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb81ce7b8ddecb3dca99",
+    "url": "/react-homework/static/js/main.8f7ac785.chunk.js"
+  },
+  {
+    "revision": "82cac3cccf9507868a58",
+    "url": "/react-homework/static/js/runtime-main.888eee4d.js"
+  }
+]);
